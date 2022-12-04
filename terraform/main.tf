f7808f71e8b94f5b620329de5b75c2085e59810c
Ubuntu <azureuser@jenkins-get-started-vm.wsn5pybwe52uhhvj0ypdzjkcqh.bx.internal.cloudapp.net>
2022-12-04 12:35:47 +0000

    create terraform

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  use_msi = true
+
+  backend "azurerm" {
+    storage_account_name = "ch2611"
+    container_name       = "tfstate-maven"
+    key                  = "prod.terraform.tfstate"
+    subscription_id      = "6c57c00d-ac27-409b-9fc2-dd266529f436"
+    tenant_id            = "7ab2df67-08b0-4840-940d-4cb97ddd5843"
+  }
+}
+resource "azurerm_resource_group" "rm" {
+  name     = "my-group-261179-rg"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "rm" {
+  name                = "alex-webapp-terraform-plan"
+  location            = azurerm_resource_group.rm.location
+  resource_group_name = azurerm_resource_group.rm.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+resource "azurerm_app_service" "rm" {
+  name                = "alex-webapp-terraform"
+  location            = azurerm_resource_group.rm.location
+  resource_group_name = azurerm_resource_group.rm.name
+  app_service_plan_id = azurerm_app_service_plan.rm.id
+}
