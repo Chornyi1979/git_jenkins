5826cb4ef67cc508780d2029fbc40f5c70067456
Ubuntu <azureuser@jenkins-get-started-vm.wsn5pybwe52uhhvj0ypdzjkcqh.bx.internal.cloudapp.net>
2022-12-04 19:44:30 +0000

    ch10

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,8 @@ terraform {
     storage_account_name = "ch2611"
     container_name       = "tfstate-maven"
     key                  = "terraform.tfstate-maven"
+    subscription_id      = "6c57c00d-ac27-409b-9fc2-dd266529f436"
+    tenant_id            = "7ab2df67-08b0-4840-940d-4cb97ddd5843"
 
    
   }
