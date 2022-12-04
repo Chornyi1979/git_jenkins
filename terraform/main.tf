4e6bf11422dcbeef042d14778d8ce5ba5c7bb9ec
Ubuntu <azureuser@jenkins-get-started-vm.wsn5pybwe52uhhvj0ypdzjkcqh.bx.internal.cloudapp.net>
2022-12-04 13:58:13 +0000

    ch2

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,7 @@ terraform {
     key                  = "prod.terraform.tfstate"
     subscription_id      = "6c57c00d-ac27-409b-9fc2-dd266529f436"
     tenant_id            = "7ab2df67-08b0-4840-940d-4cb97ddd5843"
-    features{
-    }
+    
   }
 }
   
