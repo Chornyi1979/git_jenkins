3089fbb84b1c15bedd55ccc9a31ea18ee19503e9
Ubuntu <azureuser@jenkins-get-started-vm.wsn5pybwe52uhhvj0ypdzjkcqh.bx.internal.cloudapp.net>
2022-12-07 07:51:12 +0000

    tenant rrr

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
   features {}
   
   subscription_id   = "$AZURE_SUBSCRIPTION_ID"
-  tenant_id         = "b41b72d0-4e9f-4c26-8a69-f949f367c91d"
+  tenant_id         = "$AZURE_TENANT_ID"
   client_id         = "$AZURE_CLIENT_ID"
   client_secret     = "$AZURE_CLIENT_SECRET"
 }
