a8c3365d58c828492077b9563c27f45c75b58e90
Oleksandr Chornyi <ochornyy1979@gmail.com>
2022-12-24 21:30:40 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ resource "azurerm_virtual_network" "rm" {
 }
 #  Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_linux_web_app.webapp.id
+  app_id             = azurerm_app_service.rm.id
   repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
   branch             = "master"
   use_manual_integration = true
