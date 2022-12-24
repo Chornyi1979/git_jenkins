3e09dd89c042d63337d4c14f1dd39873f4863031
Ubuntu <azureuser@jenkins-get-started-vm.wsn5pybwe52uhhvj0ypdzjkcqh.bx.internal.cloudapp.net>
2022-12-24 19:21:52 +0000

    new block

--- terraform/main.tf
+++ terraform/main.tf
@@ -55,3 +55,11 @@ resource "azurerm_virtual_network" "rm" {
   location            = azurerm_resource_group.rm.location
   address_space       = ["10.0.0.0/16"]
 }
+#  Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.webapp.id
+  repo_url           = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch             = "master"
+  use_manual_integration = true
+  use_mercurial      = false
+}
