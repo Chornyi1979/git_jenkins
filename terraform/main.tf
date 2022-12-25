19c2683cd578d0b914a8e8be63c3f650dcc9f24c
Oleksandr Chornyi <ochornyy1979@gmail.com>
2022-12-25 17:56:40 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_app_service_plan" "rm" {
   name                = "alex-maven-terraform-plan"
   location            = azurerm_resource_group.rm.location
   resource_group_name = azurerm_resource_group.rm.name
- 
+  os_type             = "Linux"
 
   sku {
     tier = "Free"
@@ -50,7 +50,8 @@ resource "azurerm_app_service" "rm" {
   app_service_plan_id = azurerm_app_service_plan.rm.id
   
   site_config {
-       linux_fx_version = "JAVA|11"    
+       linux_fx_version = "JAVA|11"
+       use_32_bit_worker_process = true
   }
   
 }
