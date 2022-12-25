47f6985a861c3d57599bbbf109b85a4e949e92e4
Oleksandr Chornyi <ochornyy1979@gmail.com>
2022-12-25 18:32:45 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,11 +45,11 @@ resource "azurerm_service_plan" "rm" {
    
 
 }
-resource "azurerm_app_service" "rm" {
+resource "azurerm_linux_web_app" "rm" {
   name                = "alex-maven-terraform"
   location            = azurerm_resource_group.rm.location
   resource_group_name = azurerm_resource_group.rm.name
-  app_service_plan_id = azurerm_service_plan.rm.id
+  service_plan_id     = azurerm_service_plan.rm.id
   
   site_config {
        linux_fx_version = "JAVA|11"
@@ -64,8 +64,8 @@ resource "azurerm_virtual_network" "rm" {
   address_space       = ["10.0.0.0/16"]
 }
 #  Deploy code from a public GitHub repo
-resource "azurerm_app_service_source_control" "sourcecontrol" {
-  app_id             = azurerm_app_service.rm.id
+resource "azurerm_linux_web_app_source_control" "sourcecontrol" {
+  app_id             = azurerm_linux_web_app.rm.id
   repo_url           = "https://github.com/Chornyi1979/git_jenkins.git"
   branch             = "main"
   use_manual_integration = true
