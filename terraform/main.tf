f8e3ee9632054c33679afd72fac4ddaacddd4f44
Oleksandr Chornyi <ochornyy1979@gmail.com>
2022-12-26 23:18:51 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,9 +46,7 @@ resource "azurerm_linux_web_app" "rm" {
   resource_group_name = azurerm_resource_group.rm.name
   service_plan_id     = azurerm_service_plan.rm.id
   
-  site_config {
-    linux_fx_version          = "JAVA|11-java11"
-  }
+  site_config {}
   
 }
 
