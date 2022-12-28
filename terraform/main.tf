aae29dd1b74dd5e80930e11cad34ec90c45ec1f1
Oleksandr Chornyi <ochornyy1979@gmail.com>
2022-12-28 23:16:49 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,12 @@ resource "azurerm_linux_web_app" "rm" {
   
   site_config {}
   
+    application_stack {
+      "java_server": "JAVA",
+      "java_server_version": 11,
+      "java_version": "java11"
+    }
+  
   
 }
 
