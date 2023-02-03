a9cf2b074afefef6dceaca34bd5c95d18f51a33f
Oleksandr Chornyi <ochornyy1979@gmail.com>
2023-02-03 14:56:20 +0200

    Update HelloTest.java

--- src/test/java/com/example/HelloTest.java
+++ src/test/java/com/example/HelloTest.java
@@ -2,44 +2,25 @@ package com.example;
 
 import com.example.model.Greeting;
 import com.example.model.User;
-import com.microsoft.azure.functions.ExecutionContext;
-import org.junit.jupiter.api.Test;
-import org.springframework.cloud.function.adapter.azure.FunctionInvoker;
-import reactor.core.publisher.Mono;
-
-import java.util.logging.Logger;
+import org.junit.Test;
+import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class HelloTest {
+public class HelloFunctionTest {
 
     @Test
     public void test() {
-        Mono<Greeting> result = new Hello().apply(Mono.just(new User("foo")));
-        assertThat(result.block().getMessage()).isEqualTo("Hello, foo!\n");
+        Greeting result = new HelloFunction().hello().apply(new User("foo"));
+        assertThat(result.getMessage()).isEqualTo("Welcome, foo");
     }
 
     @Test
-    public void start() {
-        FunctionInvoker<User, Greeting> handler = new FunctionInvoker<>(
-                Hello.class);
-        Greeting result = handler.handleRequest(new User("foo"), new ExecutionContext() {
-            @Override
-            public Logger getLogger() {
-                return Logger.getLogger(HelloTest.class.getName());
-            }
-
-            @Override
-            public String getInvocationId() {
-                return "id1";
-            }
-
-            @Override
-            public String getFunctionName() {
-                return "hello";
-            }
-        });
+    public void start() throws Exception {
+        AzureSpringBootRequestHandler<User, Greeting> handler = new AzureSpringBootRequestHandler<>(
+                HelloFunction.class);
+        Greeting result = handler.handleRequest(new User("foo"), null);
         handler.close();
-        assertThat(result.getMessage()).isEqualTo("Hello, foo!\n");
+        assertThat(result.getMessage()).isEqualTo("Welcome, foo");
     }
 }
